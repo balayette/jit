52d8abcc5d91ea9bc704fdf44865df0e6f7d6041
Nicolas Manichon <nicolas.manichon@lse.epita.fr>
2019-03-22 18:03:31 +0100

    jit: JIT calls

--- src/jit.c
+++ src/jit.c
@@ -10,11 +10,17 @@
 
 typedef int (*jited_function)(void);
 
+struct libjit_execution_unit {
+	struct libjit_ctx *context;
+	libjit_handle hdl;
+	uint8_t code[];
+};
+
 struct libjit_unit {
 	struct libjit_ast *ast;
-	bool jited;
-	void *map;
 	size_t page_count;
+	struct libjit_execution_unit *exec_unit;
+	bool jited;
 };
 
 static void *map(size_t page_count)
@@ -30,66 +36,78 @@ static void setup_unit(struct libjit_unit *unit, struct libjit_ast *ast)
 {
 	unit->ast = ast;
 	unit->jited = false;
-	unit->map = NULL;
+	unit->exec_unit = NULL;
 	unit->page_count = 0;
 }
 
-static int call_handler(libjit_handle handle)
+static int call_handler(struct libjit_ctx *ctx, libjit_handle hdl)
 {
-	(void)handle;
-	return 0;
+	return libjit_ctx_evaluate(ctx, hdl);
 }
+
+struct compile_data {
+	uint8_t *curr;
+	struct libjit_ctx *ctx;
+	struct libjit_unit *unit;
+};
+
+#define INSTR(instr, value, addr, curr)                                        \
+	curr += write_instr(instr, value, addr, curr)
 
 static void compile_node(struct libjit_ast *ast, void *user_data)
 {
-	uint8_t **data = user_data;
+	struct compile_data *cdata = user_data;
 	switch (ast->op) {
 	case ADD:
-		*data += write_instr(INSTR_POP_B, 0, *data);
-		*data += write_instr(INSTR_POP_A, 0, *data);
-		*data += write_instr(INSTR_ADD, 0, *data);
-		*data += write_instr(INSTR_PUSH_A, 0, *data);
+		INSTR(INSTR_POP_B, 0, 0, cdata->curr);
+		INSTR(INSTR_POP_A, 0, 0, cdata->curr);
+		INSTR(INSTR_ADD, 0, 0, cdata->curr);
+		INSTR(INSTR_PUSH_A, 0, 0, cdata->curr);
 		break;
 	case SUB:
-		*data += write_instr(INSTR_POP_B, 0, *data);
-		*data += write_instr(INSTR_POP_A, 0, *data);
-		*data += write_instr(INSTR_SUB, 0, *data);
-		*data += write_instr(INSTR_PUSH_A, 0, *data);
+		INSTR(INSTR_POP_B, 0, 0, cdata->curr);
+		INSTR(INSTR_POP_A, 0, 0, cdata->curr);
+		INSTR(INSTR_SUB, 0, 0, cdata->curr);
+		INSTR(INSTR_PUSH_A, 0, 0, cdata->curr);
 		break;
 	case MULT:
-		*data += write_instr(INSTR_POP_B, 0, *data);
-		*data += write_instr(INSTR_POP_A, 0, *data);
-		*data += write_instr(INSTR_MULT, 0, *data);
-		*data += write_instr(INSTR_PUSH_A, 0, *data);
+		INSTR(INSTR_POP_B, 0, 0, cdata->curr);
+		INSTR(INSTR_POP_A, 0, 0, cdata->curr);
+		INSTR(INSTR_MULT, 0, 0, cdata->curr);
+		INSTR(INSTR_PUSH_A, 0, 0, cdata->curr);
 		break;
 	case DIV:
-		*data += write_instr(INSTR_POP_B, 0, *data);
-		*data += write_instr(INSTR_POP_A, 0, *data);
-		*data += write_instr(INSTR_DIV, 0, *data);
-		*data += write_instr(INSTR_PUSH_A, 0, *data);
+		INSTR(INSTR_POP_B, 0, 0, cdata->curr);
+		INSTR(INSTR_POP_A, 0, 0, cdata->curr);
+		INSTR(INSTR_DIV, 0, 0, cdata->curr);
+		INSTR(INSTR_PUSH_A, 0, 0, cdata->curr);
 		break;
 	case CALL:
-		*data += write_instr(INSTR_PUSH_IMM, &ast->hdl, *data);
-		*data += write_instr(INSTR_POP_PARAM1, 0, *data);
-		*data += write_instr(INSTR_CALL, &call_handler, *data);
-		*data += write_instr(INSTR_PUSH_A, 0, *data);
+		INSTR(INSTR_PUSH_IMM, ast->hdl, 0, cdata->curr);
+		INSTR(INSTR_PUSH_ADDR, 0, (size_t)cdata->ctx, cdata->curr);
+		INSTR(INSTR_POP_PARAM1, 0, 0, cdata->curr);
+		INSTR(INSTR_POP_PARAM2, 0, 0, cdata->curr);
+		INSTR(INSTR_CALL, 0, (size_t)&call_handler, cdata->curr);
+		INSTR(INSTR_PUSH_A, 0, 0, cdata->curr);
 		break;
 	case ATOM:
-		*data += write_instr(INSTR_PUSH_IMM, &ast->value, *data);
+		INSTR(INSTR_PUSH_IMM, ast->value, 0, cdata->curr);
 		break;
 	}
 }
 
-static bool jit_unit(struct libjit_unit *unit)
+static bool jit_unit(struct libjit_ctx *ctx, struct libjit_unit *unit)
 {
-	uint8_t *curr = unit->map;
+	struct compile_data data = { .curr = unit->exec_unit->code,
+				     .ctx = ctx,
+				     .unit = unit };
 
-	libjit_postorder(unit->ast, compile_node, &curr);
+	libjit_postorder(unit->ast, compile_node, &data);
 
-	curr += write_instr(INSTR_POP_A, 0, curr);
-	curr += write_instr(INSTR_RET, 0, curr);
+	data.curr += write_instr(INSTR_POP_A, 0, 0, data.curr);
+	data.curr += write_instr(INSTR_RET, 0, 0, data.curr);
 
-	mprotect(unit->map, unit->page_count * PAGE_SIZE,
+	mprotect(unit->exec_unit, unit->page_count * PAGE_SIZE,
 		 PROT_READ | PROT_EXEC);
 
 	return true;
@@ -123,7 +141,7 @@ void libjit_free_ctx(struct libjit_ctx *ctx, bool free_asts)
 
 	for (size_t i = 0; i < ctx->ast_num; i++) {
 		if (ctx->units[i].jited)
-			munmap(ctx->units[i].map,
+			munmap(ctx->units[i].exec_unit,
 			       PAGE_SIZE * ctx->units[i].page_count);
 	}
 
@@ -153,7 +171,7 @@ int libjit_ctx_evaluate(struct libjit_ctx *ctx, libjit_handle hdl)
 	if (!ctx->units[hdl].jited)
 		return libjit_evaluate(ctx->units[hdl].ast);
 
-	jited_function f = (jited_function)ctx->units[hdl].map;
+	jited_function f = (jited_function)ctx->units[hdl].exec_unit->code;
 	return f();
 }
 
@@ -166,9 +184,9 @@ bool libjit_ctx_jit(struct libjit_ctx *ctx, libjit_handle hdl)
 		return true;
 
 	ctx->units[hdl].jited = false;
-	ctx->units[hdl].map = map(1);
+	ctx->units[hdl].exec_unit = map(1);
 	ctx->units[hdl].page_count = 1;
 
-	ctx->units[hdl].jited = jit_unit(ctx->units + hdl);
+	ctx->units[hdl].jited = jit_unit(ctx, ctx->units + hdl);
 	return ctx->units[hdl].jited;
 }
